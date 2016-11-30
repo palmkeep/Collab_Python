cdf5d6d7bb191c70af3ac2d136df3f687edf0125
palmkeep <andrepalmborg@gmail.com>
2016-11-30 17:21:44 +0100

    la6: Added

--- la6/test_quicksort.py
+++ la6/test_quicksort.py
@@ -1,1 +1,41 @@
+from random import randrange
+from quicksort import quicksort
 
+
+
+def check_sorted(seq):
+    if not seq or len(seq) == 1:
+        return True
+    elif seq[0] <= seq[1]:
+        return check_sorted(seq[1:])
+    else:
+        return False
+
+if __name__ == "__main__":
+    print("________________________________________")
+    print("| Checking the quicksort() function    |")
+    print("| by passing four random lists         |")
+    print("|______________________________________|\n")
+
+    num_tests = 4
+    num_int_test_seq = 20
+    while num_tests > 0:
+        i = num_int_test_seq
+        test_seq = []
+        sorted_test_seq = []
+        while i > 0:
+            test_seq.append(randrange(1,100))
+            i -= 1
+        sorted_test_seq = quicksort(test_seq)
+
+        print(test_seq)
+        print(sorted_test_seq)
+
+        assert check_sorted(sorted_test_seq) is True
+
+        print("\n")
+        num_tests -= 1
+
+    print("________________________________________")
+    print("| quicksort() passes all four tests    |")
+    print("|______________________________________|")
