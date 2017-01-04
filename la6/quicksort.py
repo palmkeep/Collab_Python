edb00f435a691f6fc18b8a7733b84c2c46fc06b7
palmkeep <andrepalmborg@gmail.com>
2017-01-04 17:24:19 +0100

    la6: Comments added to quicksort.py

--- la6/quicksort.py
+++ la6/quicksort.py
@@ -6,8 +6,19 @@
 #   vikpa137        andpa149
 
 
+"""
+quicksort() sorts a list by splitting a list of numbers into three 
+lists: the first element, all elements smaller than the first element and
+all elements equal to or greater than the first element. quicksort() is then
+called again to sort the two lists that were created and this process is
+repeated until quicksort() is being called upon empty lists.
+
+"""
 
 def quicksort(seq):
+    """
+    Sorts $seq in ascending order. Returns a new sorted list.
+    """
     if not seq:
         return []
 
