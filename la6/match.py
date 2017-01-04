7e2ff633bdc749f9bb2ebd8ee921c105b1b93a40
viktor palm <viktor palm>
2017-01-04 14:41:07 +0100

    komp match

--- la6/match.py
+++ la6/match.py
@@ -12,8 +12,10 @@ def match(pattern, seq):
     Returns whether a given sequence matches the given pattern
     """
     if not pattern:
+        #if end of the pattern check if end of seq return true else false
         return not seq
     elif pattern[0] == '--':
+        #if wildcard return true if either the rest of the patter matches the seq or the rest of the seq matches the pattern
         if match(pattern[1:], seq):
             return True
         elif not seq:
@@ -21,12 +23,16 @@ def match(pattern, seq):
         else:
             return match(pattern, seq[1:])
     elif not seq:
+        # if the sequence end before the patter ends then return false
         return False 
     elif isinstance(pattern[0], list) and isinstance(seq[0], list):
+        # if there are sub patterns check wheter those match sub seq
         return match(pattern[0], seq[0]) and match(pattern[1:], seq[1:])
     elif pattern[0] == '&':
+        #if & then skip this part of the pattern
         return match(pattern[1:], seq[1:])
     elif seq[0] == pattern[0]:
+        #if both first elements match then continue matching
         return match(pattern[1:], seq[1:])
     else:
         return False
@@ -36,6 +42,7 @@ def search(pattern, seq):
     """
     Returns all book-entries in $seq that match $pattern
     """
+    #if a book matches the patternm ad it to the final result and check next book else dont, if you are out of books then stop matching.
     if not seq:
         return []
     elif match(pattern, seq[0]):
